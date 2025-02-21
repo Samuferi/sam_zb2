489842c1b9ef2fb60f19db26641bc6e842243ab6
Samuferi <fecoo2005@gmail.com>
2025-02-21 21:57:50 +0100

    Initial commit

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='sam_zb2',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
